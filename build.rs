7dea4ef296dcc4310fde8c341244a174970dee68
descawed <github@descawed.com>
2025-12-27 03:50:52 -0500

    Only use winresource on Windows

--- build.rs
+++ build.rs
@@ -1,4 +1,9 @@
 fn main() {
+    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
+    if target_os != "windows" {
+        return;
+    }
+
     let res = winresource::WindowsResource::new();
     res.compile().unwrap();
 }
