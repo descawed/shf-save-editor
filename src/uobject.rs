c7dbadab3f82f561f62425f07238e3059c71a592
descawed <github@descawed.com>
2026-01-02 23:52:32 -0500

    Add additional type and function documentation

--- src/uobject.rs
+++ src/uobject.rs
@@ -4,7 +4,11 @@ use std::str::FromStr;
 
 use binrw::{binrw, BinRead, BinWrite, BinResult, Endian};
 
+/// A type that can be both converted to and parsed from a string.
 pub trait Stringable: ToString {
+    /// Tries to set the value of this object from the given string representation.
+    ///
+    /// If the string cannot be parsed, the value is not updated, but there is no error.
     // FromStr is not dyn compatible, so we have to go through this wrapper
     fn try_set_from_str(&mut self, s: &str);
 }
@@ -17,15 +21,20 @@ impl<T: ToString + FromStr> Stringable for T {
     }
 }
 
+/// A core Unreal Engine 5 type.
 pub trait CoreUObject: Debug {
+    /// A mutable list of fields in this object with their names.
     fn fields_mut(&mut self) -> Vec<(&'static str, &mut dyn Stringable)>;
 
+    /// The size of this object in bytes.
     fn size(&self) -> usize;
 
+    /// Converts this object to a byte vector.
     // BinWrite is not dyn compatible, so we have to go through this wrapper
     fn to_bytes(&self, endian: Endian) -> BinResult<Vec<u8>>;
 }
 
+/// Write a CoreUObject to a writer.
 #[binrw::writer(writer, endian)]
 pub fn write_uobject(object: &Box<dyn CoreUObject>) -> BinResult<()> {
     let bytes = object.to_bytes(endian)?;
@@ -40,6 +49,11 @@ fn uobject_to_bytes<'a, O: CoreUObject + BinWrite<Args<'a>=()>>(object: &O, endi
     Ok(buf)
 }
 
+/// An Unreal Engine 5 DateTime.
+///
+/// Values of this type represent dates and times between Midnight 00:00:00, January 1, 0001 and
+/// Midnight 23:59:59.9999999, December 31, 9999 in the Gregorian calendar. Internally, the time
+/// values are stored in ticks of 0.1 microseconds (= 100 nanoseconds) since January 1, 0001.
 #[binrw]
 #[derive(Debug, Clone, Copy, Default)]
 pub struct FDateTime(u64);
@@ -58,6 +72,12 @@ impl CoreUObject for FDateTime {
     }
 }
 
+/// An Unreal Engine 5 Timespan.
+///
+/// A time span is the difference between two dates and times. For example, the time span between
+/// 12:00:00 January 1, 2000 and 18:00:00 January 2, 2000 is 30.0 hours. Time spans are measured in
+/// positive or negative ticks depending on whether the difference is measured forward or backward.
+/// Each tick has a resolution of 0.1 microseconds (= 100 nanoseconds).
 #[binrw]
 #[derive(Debug, Clone, Copy, Default)]
 pub struct FTimespan(u64);
@@ -76,6 +96,7 @@ impl CoreUObject for FTimespan {
     }
 }
 
+/// An Unreal Engine 5 3D vector.
 #[binrw]
 #[derive(Debug, Clone, Default)]
 pub struct Vector {
@@ -98,6 +119,7 @@ impl CoreUObject for Vector {
     }
 }
 
+/// An Unreal Engine 5 quaternion.
 #[binrw]
 #[derive(Debug, Clone, Default)]
 pub struct Quat {
@@ -121,6 +143,7 @@ impl CoreUObject for Quat {
     }
 }
 
+/// An Unreal Engine 5 linear color (RGBA).
 #[binrw]
 #[derive(Debug, Clone, Default)]
 pub struct LinearColor {
@@ -144,6 +167,7 @@ impl CoreUObject for LinearColor {
     }
 }
 
+/// Tries to read a UE5 object of the given type from a reader.
 pub fn try_read_uobject<R: Read + Seek>(type_name: &str, reader: &mut R, endian: Endian) -> BinResult<Option<Box<dyn CoreUObject>>> {
     Ok(Some(match type_name {
         "DateTime" => Box::new(FDateTime::read_options(reader, endian, ())?),
@@ -155,6 +179,7 @@ pub fn try_read_uobject<R: Read + Seek>(type_name: &str, reader: &mut R, endian:
     }))
 }
 
+/// Creates a default value for a given Unreal Engine 5 type.
 pub fn make_default_uobject(type_name: &str) -> Option<Box<dyn CoreUObject>> {
     match type_name {
         "DateTime" => Some(Box::new(FDateTime::default())),
